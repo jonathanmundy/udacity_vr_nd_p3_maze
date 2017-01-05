6c243f6c8f82d9ebc1359ec8a85afedd6884cc78
jonathanmundy <jonathan.mundy@gmail.com>
2017-01-05 14:38:34 +0000

    feat: Instantiate and destroy KeyPoof and Door.Unlock
    Completed the Key.cs script to instantiate the KeyPoof prefab when
    clicked. Also updated the script so the GameObject is
    destroyed when clicked. Finally, linked out to Door.Unlock() from
    within the Key.OnKeyClicked() method.

--- Assets/Udacity/Scripts/Key.cs
+++ Assets/Udacity/Scripts/Key.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Key : MonoBehaviour 
 {
     //Create a reference to the KeyPoofPrefab and Door
+	public GameObject keyPoof;
+	public GameObject door;
 
 	void Update()
 	{
@@ -14,9 +16,14 @@ public class Key : MonoBehaviour
 	public void OnKeyClicked()
 	{
         // Instatiate the KeyPoof Prefab where this key is located
-        // Make sure the poof animates vertically
+		// Make sure the poof animates vertically
+		Object.Instantiate(keyPoof, transform.position, Quaternion.Euler(270, 0, 0));
+
         // Call the Unlock() method on the Door
+		door.GetComponent<Door>().Unlock();
+
         // Destroy the key. Check the Unity documentation on how to use Destroy
+		Destroy(gameObject);
     }
 
 }
