ccf51c77e8837672f9752dd8d617b8c11ac0eb47
jonathanmundy <jonathan.mundy@gmail.com>
2017-01-07 16:26:03 +0000

    feat: Complete door opening and add additional waypoints
    Completed the door opening feature (without sounds). The door is
    opened after collecting the key. Also added 2 additional waypoints
    so the player can make their way into the temple at the end of the
    game.

--- Assets/Udacity/Scripts/Door.cs
+++ Assets/Udacity/Scripts/Door.cs
@@ -8,12 +8,16 @@ public class Door : MonoBehaviour
 	public bool locked;
 
     void Update() {
-        // If the door is unlocked and it is not fully raised
-            // Animate the door raising up
-    }
+		// If the door is unlocked and it is not fully raised
+		// Animate the door raising up
+		if (!locked && transform.position.y < 26.0f) {
+			transform.Translate (0, 2.5f * Time.deltaTime, 0, Space.World);
+		}
+	}
 
     public void Unlock()
     {
         // You'll need to set "locked" to true here
+		locked = true;
     }
 }
