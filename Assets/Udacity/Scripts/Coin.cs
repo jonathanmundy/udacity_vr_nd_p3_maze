1245862e3e525debc689b12741eeceddd25ba3e7
jonathanmundy <jonathan.mundy@gmail.com>
2017-01-09 10:46:35 +0000

    style: Remove commented out code in coin.cs
    Removed commented out code in the coin.cs script. It was not being
    used in production and was cluttering up the script.

--- Assets/Udacity/Scripts/Coin.cs
+++ Assets/Udacity/Scripts/Coin.cs
@@ -9,10 +9,8 @@ public class Coin : MonoBehaviour
 
     public void OnCoinClicked() {
         // Instantiate the CoinPoof Prefab where this coin is located
+		// Make sure the poof animates vertically
 		Object.Instantiate(coinPoof, transform.position, Quaternion.Euler(270, 0, 0));
-
-        // Make sure the poof animates vertically
-		//transform.position += Vector3.up * 10.0f;
 
         // Destroy this coin. Check the Unity documentation on how to use Destroy
 		Destroy(gameObject);
